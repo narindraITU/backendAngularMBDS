1660295ff9acdccf2ed7ad82e9bfa71a44cbd69f
Mathias <mathiasravelojaona@gmail.com>
2021-04-14 17:38:43 +0300

    finishg

--- services/assignments.service.js
+++ services/assignments.service.js
@@ -1,8 +1,30 @@
 const Assignment = require('../model/assignment');
 const NotFoundException = require('../Exceptions/NotFoundException');
+const MatiereService = require('../services/matieres.service');
+const EleveService = require('../services/eleves.service');
 const DejaRenduException = require('../Exceptions/DejaRenduException');
 var ObjectId = require('mongoose').Types.ObjectId;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
 const AssignmentsService = {
+    async peupler(body){
+        let assignment = new Assignment();
+        assignment.nom = body.nom;
+        assignment.dateDeRendu = body.dateDeRendu;
+        assignment.rendu = body.rendu;
+        assignment.note = getRandomInt(0,2) === 1 ? getRandomInt(0,20) : null;
+        assignment.remarques = "";
+        const listematieres = await MatiereService.loadAll();
+        const listeEleves = await EleveService.loadAll();
+        assignment.matiere = listematieres[getRandomInt(0,listematieres.length)];
+        assignment.eleve = listeEleves[getRandomInt(0,listeEleves.length)];
+        return await assignment.save();
+    },
     async rendre(id,note,description){
         const assignment = await Assignment.findById(id);
         if(!assignment){
@@ -22,7 +44,6 @@ const AssignmentsService = {
         var aggregateQuery = Assignment.aggregate([
             {$match: {'eleve._id': new ObjectId(idEleve)}}
         ]);
-
         console.log(idEleve,aggregateQuery);
         return Assignment.aggregatePaginate(
             aggregateQuery,
